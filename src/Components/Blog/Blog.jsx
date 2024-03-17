938ba75fec4b5985269f62e014a157f86e2a95fe
tasib94 <rahi84974@gmail.com>
2024-03-18 03:53:35 +0600

    blog section almost done!

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,28 @@
 import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
-    console.log(blog)
+   const {blog_title,hashtags, Cover, author_img, author, reading_time,posted_date} = blog;
     return (
         <div>
-            
+            <img src={blog.Cover} alt={` cover picture of the title ${blog_title}`} />
+            <div className='flex justify-between'>
+                <div className='flex items-center gap-4'>
+                    <img className='w-16 h-16 rounded-full' src={blog.author_img} alt="" />
+                    <div>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{blog.reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{blog_title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a className='text-blue-700' href="">#{hash}</a></span>)
+                }
+            </p>
+
         </div>
     );
 };
